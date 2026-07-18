527b275e10864e2e9446b86e0818a3e2b4e1ed4d
Digiteach Developer <developer@digiteach.org>
2026-07-18 14:12:52 +0300

    remove unnecessary sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,8 @@ import ProgramSection from '../components/ProgramSection';
 import AboutInitiative from '../components/AboutInitiative';
 import LearningAreas from '../components/LearningAreas';
 import WhyThisMatters from '../components/WhyThisMatters';
-import Impact from '../components/Impact';
 import GetInvolved from '../components/GetInvolved';
-import Testimonials from '../components/Testimonials';
 import Partners from '../components/Partners';
-import CTA from '../components/CTA';
 import CurrentPilot from '../components/CurrentPilot';
 
 const Home = () => {
@@ -20,10 +17,7 @@ const Home = () => {
       <LearningAreas />
       <WhyThisMatters />
       <GetInvolved />
-      <Impact />
-      <Testimonials />
       <Partners />
-      <CTA />
       <CurrentPilot />
     </>
   );
